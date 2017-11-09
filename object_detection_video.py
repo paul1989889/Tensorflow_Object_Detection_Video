7e817571f5953c09ea6acc080d76628084caf033
paul1989889 <paul1989889@users.noreply.github.com>
2017-11-09 11:08:47 +0800

    create video object detection
    upload object detection with image and video

---
+++ object_detection_video.py
@@ -0,0 +1,90 @@
+# Import everything needed to edit/save/watch video clips
+import imageio
+imageio.plugins.ffmpeg.download()
+
+from moviepy.editor import VideoFileClip
+from IPython.display import HTML
+from object_detection.utils import label_map_util
+import numpy as np
+import tensorflow as tf
+import os
+from object_detection.utils import visualization_utils as vis_util
+
+
+pbPath = 'H:\DeepLeaning_test\data'
+# What model to download.
+MODEL_NAME = 'faster_rcnn_inception_resnet_v2_atrous_coco_11_06_2017'
+#DOWNLOAD_BASE = 'http://download.tensorflow.org/models/object_detection/'
+
+# Path to frozen detection graph. This is the actual model that is used for the object detection.
+PATH_TO_CKPT = MODEL_NAME + '/frozen_inference_graph.pb'
+
+# List of the strings that is used to add correct label for each box.
+PATH_TO_LABELS = os.path.join(pbPath, 'mscoco_label_map.pbtxt')
+
+NUM_CLASSES = 90
+detection_graph = tf.Graph()
+with detection_graph.as_default():
+  od_graph_def = tf.GraphDef()
+  with tf.gfile.GFile(PATH_TO_CKPT, 'rb') as fid:
+    serialized_graph = fid.read()
+    od_graph_def.ParseFromString(serialized_graph)
+    tf.import_graph_def(od_graph_def, name='')
+#Loading label map    
+label_map = label_map_util.load_labelmap(PATH_TO_LABELS)
+categories = label_map_util.convert_label_map_to_categories(label_map, max_num_classes=NUM_CLASSES, use_display_name=True)
+category_index = label_map_util.create_category_index(categories)
+
+def detect_objects(image_np, sess, detection_graph):
+    # Expand dimensions since the model expects images to have shape: [1, None, None, 3]
+    image_np_expanded = np.expand_dims(image_np, axis=0)
+    image_tensor = detection_graph.get_tensor_by_name('image_tensor:0')
+
+    # Each box represents a part of the image where a particular object was detected.
+    boxes = detection_graph.get_tensor_by_name('detection_boxes:0')
+
+    # Each score represent how level of confidence for each of the objects.
+    # Score is shown on the result image, together with the class label.
+    scores = detection_graph.get_tensor_by_name('detection_scores:0')
+    classes = detection_graph.get_tensor_by_name('detection_classes:0')
+    num_detections = detection_graph.get_tensor_by_name('num_detections:0')
+
+    # Actual detection.
+    (boxes, scores, classes, num_detections) = sess.run(
+        [boxes, scores, classes, num_detections],
+        feed_dict={image_tensor: image_np_expanded})
+
+    # Visualization of the results of a detection.
+    vis_util.visualize_boxes_and_labels_on_image_array(
+        image_np,
+        np.squeeze(boxes),
+        np.squeeze(classes).astype(np.int32),
+        np.squeeze(scores),
+        category_index,
+        use_normalized_coordinates=True,
+        line_thickness=8)
+    return image_np
+
+def process_image(image):
+    # NOTE: The output you return should be a color image (3 channel) for processing video below
+    # you should return the final output (image with lines are drawn on lanes)
+    with detection_graph.as_default():
+        with tf.Session(graph=detection_graph) as sess:
+            image_process = detect_objects(image, sess, detection_graph)
+            return image_process
+        
+white_output = 'H:/DeepLearningData/out/demo_out.mp4'
+clip1 = VideoFileClip("H:/DeepLearningData/in/demo.mp4").subclip(0, 15)
+
+white_clip = clip1.fl_image(process_image) #NOTE: this function expects color images!!s
+white_clip.write_videofile(white_output, audio=False)
+
+HTML("""
+<video width="960" height="540" controls>
+  <source src="{0}">
+</video>
+""".format(white_output))
+
+
+clip1 = VideoFileClip("H:/DeepLearningData/out/demo_out.mp4")
+clip1.write_gif("H:/DeepLearningData/out/final.gif")
